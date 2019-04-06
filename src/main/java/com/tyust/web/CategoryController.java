8e4d2307bccb22a44d5d4147683cc2ff518b96b4
Diusha0722 <41934662+DiushaAndNiusha@users.noreply.github.com>
2019-04-06 22:30:12 +0800

    第一次提交

---
+++ src/main/java/com/tyust/web/CategoryController.java
@@ -0,0 +1,26 @@
+package com.tyust.web;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.tyust.entity.Category;
+import com.tyust.service.CategoryService;
+
+@Controller
+@RequestMapping("category")
+public class CategoryController {
+	@Autowired
+	private CategoryService categoryService;
+	
+	//左侧的侧边栏
+	@RequestMapping("/findAll.do")
+	public String findAll(ModelMap map){
+		List<Category> parents = categoryService.findAll();
+		map.addAttribute("parents", parents);
+		return "/jsps/left";
+	}
+}
